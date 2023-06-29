b5ad5bc115b25c16d55e04a6ec6ea5d1e7fff09b
Lans12345 <olanalans12345@gmail.com>
2023-06-29 19:20:23 +0800

    updated title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "LANCE OLANA",
+      title: "Portfolio - Lance Olana",
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       darkTheme: Theme.of(context).copyWith(
